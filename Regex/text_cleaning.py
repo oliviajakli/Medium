1e536fa662435d0f39b315cfcfe1596fe25766af
Olivia Jakli <jakli.olivia@gmail.com>
2025-06-06 01:27:55 -0400

    Demonstrate text cleaning on product description

--- Regex/text_cleaning.py
+++ Regex/text_cleaning.py
@@ -1,3 +1,23 @@
 # Author: Olivia Jákli
-# Example #2 for Medium article on real-world use cases for regular expressions.
+# Example #3 for Medium article on real-world use cases for regular expressions.
+import re
 
+product_description = """
+<div class="long-description">
+    <p><strong>Amazing</strong> sample description of an overpriced product with <em>5-star reviews!!!</em></p>
+</div>
+"""
+
+# Remove HTML tags.
+cleaned_description = re.sub(r'<[^>]+>', '', product_description)
+
+# Replace HTML character entities.
+cleaned_description = re.sub(r'&[a-ZA-Z]', '', cleaned_description)
+
+# Remove special characters while keeping basic punctuation.
+cleaned_description = re.sub(r'[^\w\s.,!?-]', '', cleaned_description)
+
+# Normalize whitespace.
+cleaned_description = re.sub(r'\s+', ' ', cleaned_description).strip()
+
+print(cleaned_description)
