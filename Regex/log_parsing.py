b950e82d36075fcd8f37b45e019195f678347fc7
Olivia Jakli <jakli.olivia@gmail.com>
2025-06-07 17:34:55 -0400

    Example 5: Log Parsing

---
+++ Regex/log_parsing.py
@@ -0,0 +1,18 @@
+# Author: Olivia Jákli
+# Example #5
+# Parse a log file for specific error messages using regex.
+import re
+
+log = """
+2025-01-01 10:30:45 ERROR: Database connection failed
+2025-01-01 10:31:00 INFO: User logged in successfully
+2025-01-01 10:32:15 WARNING: Low disk space
+2025-01-01 10:33:20 ERROR: File not found
+2025-01-01 10:34:05 INFO: Data backup completed
+2025-01-01 10:35:30 ERROR: Connection timeout after 30 seconds
+2025-01-01 10:36:00 INFO: System rebooted
+2025-01-01 10:37:15 ERROR: SSL connection failed
+"""
+
+if re.search(r'ERROR.*connection', log):
+    print("An error related to connection was found in the log.")
