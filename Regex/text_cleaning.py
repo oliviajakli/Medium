acd650746d37d3d236ddee49f473780fa44fed82
Olivia Jakli <jakli.olivia@gmail.com>
2025-06-08 19:33:42 -0400

    Created a function, Example 5: Text Cleaning

--- Regex/text_cleaning.py
+++ Regex/text_cleaning.py
@@ -3,22 +3,28 @@
 # Clean a product description by removing HTML tags and character entities, and special characters.
 import re
 
+
 product_description = """
 <div class="long-description">
     <p><strong>Amazing</strong> sample description of  an overpriced product&trade; with <em>5-star reviews!!!</em></p>
 </div>
 """
 
-# Remove HTML tags.
-cleaned_description = re.sub(r'<[^>]+>', '', product_description)
+def clean_product_description(product_description):
+    
+    # Remove HTML tags.
+    cleaned_description = re.sub(r'<[^>]+>', '', product_description)
+    
+    # Replace HTML character entities.
+    cleaned_description = re.sub(r'&[a-z]+;', '', cleaned_description)
+    
+    # Remove special characters while keeping basic punctuation.
+    cleaned_description = re.sub(r'[^\w\s.,!?-]', '', cleaned_description)
+    
+    # Normalize whitespace.
+    cleaned_description = re.sub(r'\s+', ' ', cleaned_description).strip()
+    
+    return cleaned_description
 
-# Replace HTML character entities.
-cleaned_description = re.sub(r'&[a-z];', '', cleaned_description)
 
-# Remove special characters while keeping basic punctuation.
-cleaned_description = re.sub(r'[^\w\s.,!?-]', '', cleaned_description)
-
-# Normalize whitespace.
-cleaned_description = re.sub(r'\s+', ' ', cleaned_description).strip()
-
-print(cleaned_description)
+print(clean_product_description(product_description))
