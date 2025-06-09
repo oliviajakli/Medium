2cd8fefdfa7189fe61e869f13dc8a44746f4d2b2
Olivia Jakli <jakli.olivia@gmail.com>
2025-06-08 20:09:22 -0400

    Updated product description and HTML entity removal, Example 5

--- Regex/text_cleaning.py
+++ Regex/text_cleaning.py
@@ -6,7 +6,8 @@ import re
 
 product_description = """
 <div class="long-description">
-    <p><strong>Amazing</strong> sample description of  an overpriced product&trade; with <em>5-star reviews!!!</em></p>
+    <p><strong>Amazing</strong> and&amp; creative  sample description of  a \
+    product&trade; with <em>5-star***** reviews!!!</em></p>
 </div>
 """
 
@@ -15,8 +16,8 @@ def clean_product_description(product_description):
     # Remove HTML tags.
     cleaned_description = re.sub(r'<[^>]+>', '', product_description)
     
-    # Replace HTML character entities.
-    cleaned_description = re.sub(r'&[a-z]+;', '', cleaned_description)
+    # Remove HTML character entities.
+    cleaned_description = re.sub(r'&[a-zA-Z]+;', '', cleaned_description)
     
     # Remove special characters while keeping basic punctuation.
     cleaned_description = re.sub(r'[^\w\s.,!?-]', '', cleaned_description)
