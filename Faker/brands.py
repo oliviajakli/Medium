0b2772a446b344ba0e1bd7214d34361f7f5182ae
Olivia Jakli <jakli.olivia@gmail.com>
2025-08-20 08:57:40 -0400

    Fixed: number of units sold cannot exceed number of units received

--- Faker/brands.py
+++ Faker/brands.py
@@ -1,30 +1,41 @@
 from faker import Faker
 from faker.providers import DynamicProvider
 import pandas as pd
+import random
 
-fashion_brand_names = DynamicProvider(
-    provider_name = "fashion_brand",
-    elements = [
-        "Gucci", "Balenciaga", "Valentino", "Rick Owens",
-        "Willy Chavarria", "Bottega Veneta", "Sacai", "Libertine",
-        "Alaia", "Saint Laurent", "Aya Muse", "Dries Van Noten",
-        "Givenchy", "Maison Margiela", "Kelly Cole"]
-)
+def generate_brand_names():
+    # Define a custom list of fashion brand names.
+    fashion_brand_names = DynamicProvider(
+        provider_name = "fashion_brand",
+        elements = [
+            "Gucci", "Balenciaga", "Valentino", "Rick Owens",
+            "Willy Chavarria", "Bottega Veneta", "Sacai", "Libertine",
+            "Alaia", "Saint Laurent", "Aya Muse", "Dries Van Noten",
+            "Givenchy", "Maison Margiela", "Kelly Cole"
+            ]
+    )
 
-fake = Faker()
+    fake = Faker()
 
-fake.add_provider(fashion_brand_names)
+    fake.add_provider(fashion_brand_names)
 
-# Return a unique, random brand name from the list.
-names = [fake.unique.fashion_brand() for _ in range(10)]
-assert len(set(names)) == len(names), "Duplicate brand names found!"
+    # Return a unique, random brand name from the list.
+    names = [fake.unique.fashion_brand() for _ in range(10)]
+    assert len(set(names)) == len(names), "Duplicate brand names found!"
+    return names
 
-num_records = 10
+
+def generate_inventory_data(num_records, min_received=1, max_received=100):
+    units_received = [random.randint(min_received, max_received) for _ in range(num_records)]
+    units_sold = [random.randint(0, received) for received in units_received]
+    return units_received, units_sold
+
+received, sold = generate_inventory_data(10)
 
 data = {
-    'brand_name': names,
-    'units_sold': [fake.random_int(min=0, max=100) for _ in range(num_records)],
-    'units_received': [fake.random_int(min=1, max=100) for _ in range(num_records)]
+    'brand_name': generate_brand_names(),
+    'units_received': received,
+    'units_sold': sold
 }
 
 df_faker = pd.DataFrame(data)
